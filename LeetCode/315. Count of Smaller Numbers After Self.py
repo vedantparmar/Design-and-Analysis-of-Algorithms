b75b85e1d612d385d48f21f1294fd4416da5ea58
vedantparmar <67739021+vedantparmar@users.noreply.github.com>
2024-11-12 22:07:32 +0530

    Create 315. Count of Smaller Numbers After Self.py

---
+++ LeetCode/315. Count of Smaller Numbers After Self.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+
+        arr, ans = sorted(nums), []           #  <-- 1)
+        
+        for num in nums:
+            i = bisect_left(arr,num)          #  <-- 2a)
+            ans.append(i)                     #  <-- 2b)
+            del arr[i]                        #  <-- 2c)
+            
+        return ans                            #  <-- 3)class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+      my_dict = []
+      # Calulating Distance using Distance Fromula
+      for i in points:
+        y = (i[0]**2+i[1]**2)**0.5
+        y = [i,y]
+        my_dict.append(y)
+      # Sorting the values on the basis of their distance from Orign in ascending order
+      my_dict.sort(key = lambda x:x[1])
+      x = my_dict[0][1]
+      j = 0
+      z = []
+      # Getting K closest point from Orign
+      while j<k:
+        z.append(my_dict[j][0])
+        j+=1
+      return z
+
+
+      
+        
